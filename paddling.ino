b884be72940b13aa788ab760a30c17e0a26c596b
Edward Terry <edward.william.terry@gmail.com>
2023-11-04 08:48:34 -0600

    As-used Arduino file

--- paddling.ino
+++ paddling.ino
@@ -1,5 +1,6 @@
 unsigned long time = 0;
 
+const char button = 4;
 const char echoLeft = 9;
 const char trigLeft = 10;
 const char echoRight = 11;
@@ -7,6 +8,10 @@ const char trigRight = 12;
 
 const char leftIndex = 0;
 const char rightIndex = 1;
+
+unsigned long debounceDuration = 50; // millis
+unsigned long lastTimeButtonStateChanged = 0;
+byte lastButtonState = LOW;
 
 long readUltrasonic(const char side) {
   char echo;
@@ -16,9 +21,11 @@ long readUltrasonic(const char side) {
     case 0:
       echo = echoLeft;
       trig = trigLeft;
+      break;
     case 1:
       echo = echoRight;
       trig = trigRight;
+      break;
   }
 
   digitalWrite(trig, LOW);
@@ -36,20 +43,39 @@ void setup() {
   pinMode(trigRight, OUTPUT);
   pinMode(echoLeft, INPUT);
   pinMode(echoRight, INPUT);
+  pinMode(button, INPUT_PULLUP);
 
   Serial.begin(9600);
+}
+
+bool isButtonPressed() {
+  bool result = false;
+    if (millis() - lastTimeButtonStateChanged > debounceDuration) {
+      byte buttonState = digitalRead(button);
+      if (buttonState != lastButtonState) {
+      lastTimeButtonStateChanged = millis();
+      lastButtonState = buttonState;
+      if (buttonState == LOW) {
+        result = true;
+      }
+    }
+  }
+  return result;
 }
 
 void loop() {
   time = millis();
   long distanceLeft = readUltrasonic(leftIndex);
   long distanceRight = readUltrasonic(rightIndex);
-
+  // delay(20);
+  bool isPressed = isButtonPressed();
   Serial.print(time);
   Serial.print(",");
   Serial.print(distanceLeft);
   Serial.print(",");
   Serial.print(distanceRight);
+  Serial.print(",");
+  Serial.print(isPressed);
   Serial.println();
 }
 
